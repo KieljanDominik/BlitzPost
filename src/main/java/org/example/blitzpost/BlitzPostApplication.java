475574c2dab210c3cb7fa5a0295e2b1d0ded9032
KieljanDominik <kieljandominik@gmail.com>
2024-05-02 23:41:23 +0200

    #001-Initialize of project

---
+++ src/main/java/org/example/blitzpost/BlitzPostApplication.java
@@ -0,0 +1,13 @@
+package org.example.blitzpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlitzPostApplication.class, args);
+    }
+
+}
